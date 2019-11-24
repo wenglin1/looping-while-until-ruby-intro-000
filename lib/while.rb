af19df4fff7151be5e395f45150e5b48fd0d0771
weng lin <wenglin@buffalo.edu>
2019-11-24 09:12:53 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,5 @@
-def using_while(levitation_force)
-  levitation_force = 6
-  while levitation_force < 10 do
-    puts "Wingardium Leviosa"
-    levitation_force += 1
-  end
+levitation_force = 6
+while levitation_force < 10 do
+  puts "Wingardium Leviosa"
+  levitation_force += 1
 end
