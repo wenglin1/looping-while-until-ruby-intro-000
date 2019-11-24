fac8422e1e6b9e3302f344ff5530cb16f6801ba2
weng lin <wenglin@buffalo.edu>
2019-11-24 09:13:21 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,5 @@
-def using_until(levitation_force)
-  levitation_force = 6
-  until levitation_force == 10 do
-    puts "Wingardium Leviosa"#your code here
-    levitation_force += 1
-  end
+levitation_force = 6
+until levitation_force == 10 do
+  puts "Wingardium Leviosa"#your code here
+  levitation_force += 1
 end
